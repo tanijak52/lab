c99349481c3f26b1e806560a081a4419c3fbbc75
tanijak52 <tatuana7887yy@gmail.com>
2025-05-23 21:54:33 +0200

    add 2 class

--- main.py
+++ main.py
@@ -28,12 +28,39 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x =  player_x
         self.rect.y = player_y
-
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys_pressed[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+        self.reset()
+class Enemy(GameSprite):
+    def update(self):
+        if self.rect.x <= 470:
+            self.direction = "right"
+        if self.direction == "right":
+            self.rect.x += self.speed
+        if self.rect.x <= 470:
+            self.direction = "left"
+        if self.direction == "left":
+             self.rect.x -= self.speed
+    
+   
 player = GameSprite("hero.png", 100, 100, 5)
 enemy = GameSprite("cyborg.png", 300, 100, 2)
 money = GameSprite("treasure.png", 500, 100, 0)
+
+
+
 
 while game:
     for e in event.get():
