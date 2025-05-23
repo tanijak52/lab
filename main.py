98eee479aa6182640d90cc7dd11124937e0f732f
tanijak52 <tatuana7887yy@gmail.com>
2025-05-23 16:19:15 +0200

    add code

---
+++ main.py
@@ -0,0 +1,49 @@
+from pygame import *
+
+
+win_width = 700
+win_height = 500
+
+window = display.set_mode((win_width, win_height))
+display.set_caption("Maze")
+background = transform.scale(image.load("background.jpg"), (win_width, win_height))
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+
+mixer.init()
+mixer.music.load("jungles.ogg")
+mixer.music.play()
+
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x =  player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+player = GameSprite("hero.png", 100, 100, 5)
+enemy = GameSprite("cyborg.png", 300, 100, 2)
+money = GameSprite("treasure.png", 500, 100, 0)
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+            
+    window.blit(background, (0, 0))
+    clock.tick(FPS)
+    player.reset()
+    enemy.reset()
+    money.reset()
+
+    display.update()
