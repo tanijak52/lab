53a6ee9ef6fccb21df1c6dc06555d51c08758b56
tanijak52 <tatuana7887yy@gmail.com>
2025-05-30 20:22:00 +0200

    add class wall

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ mixer.init()
 mixer.music.load("jungles.ogg")
 mixer.music.play()
 
+money = mixer.Sound("money.ogg")
+kick = mixer.Sound("kick.ogg")
+
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
@@ -59,24 +62,51 @@ class Enemy(GameSprite):
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-
+class Wall(sprite.Sprite):
+    def __init__(self, wall_x, wall_y, color):
+        super().__init__()
+        self.image = Surface((200, 10))
+        self.image.fill((0,0,255))
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
+        
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 player = Player("hero.png", 100, 100, 5)      
 enemy = Enemy("cyborg.png", 300, 100, 2)      
 money = GameSprite("treasure.png", 500, 100, 0)
+wall1 = Wall(500, 70, (0, 0, 255))
+wall2 = Wall(200, 90, (0, 0, 255))
+wall3 = Wall(600, 40, (0, 0, 255))
+wall4 = Wall(800, 50, (0, 0, 255))
+wall5 = Wall(210, 40, (0, 0, 255))
 
+walls = [wall1, wall2, wall3, wall4, wall5]
+
+font.init()
+font = font.Font(None, 70)
+win = font.render("YOU WIN", True, (255,215, 0))
+lose = font.render("YOU LOSE", False, (255,215, 0))
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-
     window.blit(background, (0, 0))
+    for wall in walls:
+        wall.draw_wall()
+    if finish != True:
+       if sprite.collide_rect(player, money):
+           window.blit(background, (200, 200))
+           finish = True
+           money.play()
     clock.tick(FPS)
 
     player.update()   
     enemy.update()    
     enemy.reset()
     money.reset()
-
+    
     display.update()
