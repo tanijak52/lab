937bee4cc3ea7405d3a53ac32a54e208a46a7097
tanijak52 <tatuana7887yy@gmail.com>
2025-05-30 16:41:39 +0200

    add fix code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pygame import *
 
-
 win_width = 700
 win_height = 500
 
@@ -19,14 +18,13 @@ mixer.music.load("jungles.ogg")
 mixer.music.play()
 
 
-
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (65, 65))
         self.speed = player_speed
         self.rect = self.image.get_rect()
-        self.rect.x =  player_x
+        self.rect.x = player_x
         self.rect.y = player_y
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
@@ -43,33 +41,41 @@ class Player(GameSprite):
         if keys_pressed[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
         self.reset()
+
 class Enemy(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__(player_image, player_x, player_y, player_speed)
+        self.direction = "left" 
+
     def update(self):
-        if self.rect.x <= 470:
-            self.direction = "right"
-        if self.direction == "right":
-            self.rect.x += self.speed
-        if self.rect.x <= 470:
-            self.direction = "left"
         if self.direction == "left":
-             self.rect.x -= self.speed
-    
-   
-player = GameSprite("hero.png", 100, 100, 5)
-enemy = GameSprite("cyborg.png", 300, 100, 2)
+            self.rect.x -= self.speed
+            if self.rect.x <= 470:
+                self.direction = "right"
+        else:
+            self.rect.x += self.speed
+            if self.rect.x >= 620:
+                self.direction = "left"
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+player = Player("hero.png", 100, 100, 5)      
+enemy = Enemy("cyborg.png", 300, 100, 2)      
 money = GameSprite("treasure.png", 500, 100, 0)
-
-
 
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-            
+
     window.blit(background, (0, 0))
     clock.tick(FPS)
-    player.reset()
+
+    player.update()   
+    enemy.update()    
     enemy.reset()
     money.reset()
 
